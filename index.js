5b246ab104f4d9d1383274e2058bdd57d299abf8
/VEX <ahmedeopop@gmail.com>
2026-08-07 08:24:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { NewMessage } from 'telegram/events/index.js'
 import { createReadStream } from 'fs'
 
 // 🆔 ايدي المطور
-const DEVELOPER_CHAT_ID = ["7248282408","8858294893"];
+const DEVELOPER_CHAT_ID = 7248282408;
 
 let db = await loadDB()
 
